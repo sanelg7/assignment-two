2a87ec48d9154bc97deec01a22c8d23a764b593a
Ege Şanel <sanelege@gmail.com>
2023-01-30 00:28:07 +0300

    initial commit, created maven project with starter archetype

---
+++ src/main/java/org/definex/practicum/App.java
@@ -0,0 +1,13 @@
+package org.definex.practicum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
