9c0ef2caad09b8319999e1ed371b71b81ce3574e
Ege Şanel <sanelege@gmail.com>
2023-01-30 19:19:33 +0300

    Improved Product class with a unique id. Will help later on with implied Customer <-> Vendor relationship

--- src/main/java/org/definex/practicum/product/Product.java
+++ src/main/java/org/definex/practicum/product/Product.java
@@ -2,12 +2,17 @@ package org.definex.practicum.product;
 
 public class Product {
 
+    private static int productIdCounter = 0;
+
+    private int productId;
     private String name;
     private double price;
 
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
+
+        productId = productIdCounter++;
     }
 
     public String getName() {
@@ -24,5 +29,18 @@ public class Product {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
     }
 }
