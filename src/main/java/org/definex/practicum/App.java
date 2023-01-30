afa13d45099b8b51104ee4b7e3f4d45acdd81ded
Ege Şanel <sanelege@gmail.com>
2023-01-30 19:02:16 +0300

    Completed initial Invoice implementation and tested id auto increment

--- src/main/java/org/definex/practicum/App.java
+++ src/main/java/org/definex/practicum/App.java
@@ -1,7 +1,11 @@
 package org.definex.practicum;
 
+import org.definex.practicum.invoice.Invoice;
 import org.definex.practicum.user.Customer;
 import org.definex.practicum.user.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Hello world!
@@ -12,6 +16,14 @@ public class App
     public static void main( String[] args ) {
 
         User user = new Customer();
+        List<Invoice> invoices = new ArrayList<>();
+        for (int i= 0;i<3;i++) {
+            Invoice invoice = new Invoice();
+            invoices.add(invoice);
+        }
+        for (Invoice i:invoices) {
+            System.out.println(i);
+        }
 
 
     }
--- src/main/java/org/definex/practicum/invoice/Invoice.java
+++ src/main/java/org/definex/practicum/invoice/Invoice.java
@@ -7,9 +7,56 @@ import java.util.HashMap;
 
 public class Invoice {
 
-    private static int invoiceId;
+    //  Provided static value to mimic an auto increment operation on invoiceId.
+    private static int invoiceIdCounter = 0;
+    private int invoiceId;
     private LocalDateTime date;
     private HashMap<Product, Integer> products;
     private int totalPrice;
 
+    public Invoice() {
+        invoiceId = invoiceIdCounter++;
+    }
+
+    public Invoice(LocalDateTime date, HashMap<Product, Integer> products) {
+        this.date = date;
+        this.products = products;
+
+        invoiceId = invoiceIdCounter++;
+
+    }
+
+    public int getInvoiceId() {
+        return invoiceId;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public HashMap<Product, Integer> getProducts() {
+        return products;
+    }
+
+    public void setProducts(HashMap<Product, Integer> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "invoiceId=" + invoiceId +
+                ", date=" + date +
+                ", products=" + products +
+                ", totalPrice=" + totalPrice +
+                '}';
+    }
 }
